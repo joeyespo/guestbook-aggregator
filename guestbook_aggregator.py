37566d9393e8236e327b84aefaea1f6c1ab9ca9d
Joe Esposito <joe@joeyespo.com>
2014-08-27 11:53:28 -0400

    Fix collection view, add new, and handle errors.

--- guestbook_aggregator.py
+++ guestbook_aggregator.py
@@ -1,11 +1,9 @@
 from __future__ import print_function
 
-import urllib
-import hashlib
 from urlparse import urljoin
 from dateutil import parser
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 
 # Flask application
@@ -19,35 +17,78 @@ API_URLS = app.config['GUESTBOOK_API_URLS'].split(';')
 # Views
 @app.route('/')
 def index():
-    entries = aggregate_entries()
-    return render_template('index.html', entries=entries)
+    try:
+        entries = aggregate_entries()
+        error = None
+    except requests.exceptions.ConnectionError as ex:
+        entries = []
+        error = 'Error getting entries: ' + str(ex)
+    return render_template('index.html', entries=entries, error=error)
+
+
+@app.route('/<entry>')
+def view_entry(id):
+    entry = load_entry(id)
+    return render_template('entries/view.html', entry=entry)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_entry():
+    if (request.method == 'GET'):
+        return render_template('entries/new.html', urls=API_URLS)
+
+    url = urljoin(request.form['url'], '/entries')
+    payload = {
+        'name': request.form['name'],
+        'email': request.form['email'],
+        'message': request.form['message'],
+    }
+    r = requests.post(url, data=payload)
+    if not r.ok:
+        return r.text, r.status_code
+
+    return redirect(url_for('index'))
 
 
 # Helpers
+def load_entry(guestbook_url, id):
+    url = urljoin(guestbook_url, '/entry/' + str(id))
+
+    # Get entry from guestbook
+    try:
+        r = requests.get(url)
+        return r.json()
+    except requests.exceptions.ConnectionError as ex:
+        # Show an error as an entry
+        return {
+            'name': type(ex),
+            'photo': url_for('static', filename='error.png'),
+            'message': str(ex),
+        }
+
+
 def aggregate_entries():
     urls = map(lambda url: urljoin(url, '/entries'), API_URLS)
+
+    # Get all entries from all guestbooks
     entries = []
     for url in urls:
-        r = requests.get(url)
-        # TODO: handle errors
-        data = r.json()
-        entries += map(transform_entry, data['entries'])
-    entries.sort(key=lambda x: x['timestamp'], reverse=True)
+        try:
+            r = requests.get(url)
+            data = r.json()
+            entries += data['entries']
+        except requests.exceptions.ConnectionError as ex:
+            # Show an error as an entry
+            entries.append({
+                'name': type(ex),
+                'photo': url_for('static', filename='error.png'),
+                'message': str(ex),
+            })
+
+    # Sort on the timestamp, if available
+    entries.sort(key=lambda e: parser.parse(e['timestamp']) if e.get('timestamp') else None, reverse=True)
+
     return entries
-
-
-def transform_entry(entry):
-    return {
-        'timestamp': parser.parse(entry['timestamp']),
-        'author': entry['author'],
-        'photo': gravatar(entry['author']),
-        'message': entry['message'],
-    }
-
-
-def gravatar(email):
-    email_hash = hashlib.md5(email.lower()).hexdigest()
-    return 'http://www.gravatar.com/avatar/' + email_hash
 
 
 # Run development server
